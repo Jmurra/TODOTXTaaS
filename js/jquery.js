46a5aa1610be49380fcd343483d911abae8ba0ca
Jerry Murray <Jmurray1325@gmail.com>
2015-04-11 19:22:32 -0400

    added jquery add function

---
+++ js/jquery.js
@@ -0,0 +1,9 @@
+// $.get( "ajax/index.html" , function (data) {
+//  $( ".result" ) .html ( data );
+// });
+
+$("#addItem").click( function () {
+  $("#incomplete").append("<li>blah</li>");
+  //make a varaible with list item html
+  //append that variable to incomplete list.
+});
